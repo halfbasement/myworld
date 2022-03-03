65ff0d25641c878c7ec7fe33ce4de64df9aad87d
halfbasement <anyang0710@naver.com>
2022-03-04 04:06:49 +0900

    test delete

--- src/main/java/com/sik/myworld/config/SecurityConfig.java
+++ src/main/java/com/sik/myworld/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.sik.myworld.config;
 
-import com.sik.myworld.security.filter.ApiCheckFilter;
 import com.sik.myworld.security.handler.MemberFailureHandler;
 import com.sik.myworld.security.handler.MemberLoginSuccessHandler;
 import com.sik.myworld.security.service.MemberUserDetailService;
@@ -8,13 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @Slf4j
